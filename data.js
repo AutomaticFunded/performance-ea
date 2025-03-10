2a488b96a5c32e110f34546eb6bf395c0a7b317f
AutomaticFunded <corcellad02@gmail.com>
2025-03-10 11:57:50 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+const performanceData = {
+    "cliente123": {
+        balance: 110307.90,
+        totalResult: 10307.90,
+        totalResultPercent: 10.30,
+        yesterdayResult: 0.29,
+        yesterdayResultValue: 302,
+        yesterdayDrawdown: 0.00,
+        yesterdayDrawdownValue: 0,
+        float: 0,
+        orders: 0,
+        profitFactor: 1.87,
+        winRate: 90,
+        averageWin: 8,
+        averageLoss: -39,
+        maxDrawdown: -1.24,
+        maxDrawdownValue: -1282,
+        totalOrders: 1535,
+        chartData: [0, 5, 10, 15, 20, 25, 30], // Percentuali profitto ultimi 30 giorni
+        trades: [
+            { date: "31 Mar", trades: 0, result: 0, percent: 0.00 },
+            { date: "30 Mar", trades: 46, result: -302, percent: -0.29 },
+            { date: "29 Mar", trades: 37, result: -102, percent: -0.10 },
+            { date: "28 Mar", trades: 27, result: -186, percent: -0.17 },
+            { date: "27 Mar", trades: 40, result: -311, percent: -0.28 },
+            { date: "26 Mar", trades: 0, result: 0, percent: 0.00 },
+            { date: "25 Mar", trades: 0, result: 0, percent: 0.00 },
+            { date: "24 Mar", trades: 2, result: 0, percent: 0.00 },
+            { date: "23 Mar", trades: 72, result: 208, percent: 0.19 },
+            { date: "22 Mar", trades: 8, result: 0, percent: 0.00 }
+        ]
+    },
+    "cliente456": {
+        balance: 50000.00,
+        totalResult: 5000.00,
+        totalResultPercent: 10.00,
+        yesterdayResult: 0.15,
+        yesterdayResultValue: 75,
+        yesterdayDrawdown: 0.00,
+        yesterdayDrawdownValue: 0,
+        float: 0,
+        orders: 0,
+        profitFactor: 1.50,
+        winRate: 85,
+        averageWin: 5,
+        averageLoss: -30,
+        maxDrawdown: -2.00,
+        maxDrawdownValue: -1000,
+        totalOrders: 1200,
+        chartData: [0, 3, 6, 9, 12, 15, 18],
+        trades: [
+            { date: "31 Mar", trades: 0, result: 0, percent: 0.00 },
+            { date: "30 Mar", trades: 20, result: -150, percent: -0.30 },
+            { date: "29 Mar", trades: 15, result: -80, percent: -0.16 }
+        ]
+    }
+};
